911cbd95f47dea6b22bd4ee5c1ba8ad521387bb4
Devarajan21294 <devaraj21294@gmail.com>
2026-07-31 12:43:41 +0530

    Delete NAUKRI_PROJECT directory

--- NAUKRI_PROJECT/src/main/java/com/interfaceelements/loginPageInterfaceElements.java
+++
@@ -1,9 +0,0 @@
-package com.interfaceelements;
-
-public interface loginPageInterfaceElements {
-	String username_id="usernameField";
-	String password_css="input#passwordField";
-	String loginbtn_xpath="//button[text()=\"Login\"]";
-	String title_xpath="//div[@title=\"Devarajan Sadhasivam\"]";
-
-}
